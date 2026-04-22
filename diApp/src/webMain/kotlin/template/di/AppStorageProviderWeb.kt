63d9092b4dce7afdfa4b23d7342cf0c0b510d2ae
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2026-04-22 19:29:42 +0200

    Update DataStore dependency version

--- diApp/src/webMain/kotlin/template/di/AppStorageProviderWeb.kt
+++ diApp/src/webMain/kotlin/template/di/AppStorageProviderWeb.kt
@@ -2,17 +2,15 @@ package template.di
 
 import androidx.datastore.core.Storage
 import androidx.datastore.core.okio.OkioSerializer
-import androidx.datastore.core.okio.WebStorage
-import androidx.datastore.core.okio.WebStorageType
+import androidx.datastore.core.okio.WebOpfsStorage
 import org.koin.core.annotation.Single
 import template.core.pref.StorageProvider
 
 @Single
 internal class AppStorageProviderWeb : StorageProvider {
     override fun <T> getStorage(serializer: OkioSerializer<T>): Storage<T> =
-        WebStorage(
+        WebOpfsStorage(
             serializer = serializer,
             name = "app.pref.json",
-            storageType = WebStorageType.LOCAL,
         )
 }
