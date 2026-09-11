46659dddbecb8900a34319283dfa6dbe62e2b5a0
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2026-09-11 10:02:42 +0200

    build(deps): update Compose Multiplatform to 1.13.0-alpha01 and simplify JS tests

--- diApp/src/commonTest/kotlin/template/di/KoinAppCommonTest.kt
+++ diApp/src/commonTest/kotlin/template/di/KoinAppCommonTest.kt
@@ -3,19 +3,23 @@ package template.di
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.printToLog
 import androidx.compose.ui.test.v2.runComposeUiTest
 import kotlin.test.Test
 
 @OptIn(ExperimentalTestApi::class)
 internal class KoinAppCommonTest {
     @Test
-    fun simpleCheck() =
+    fun simpleCheck() {
         runComposeUiTest {
+            mainClock.autoAdvance = false
             setContent { KoinApp() }
+            onNodeWithText("Splash:", substring = true).assertIsDisplayed()
+
+            mainClock.autoAdvance = true
             waitForIdle()
-            onRoot().printToLog("StartTag")
+            awaitIdle()
+
             onNodeWithText("No document saved yet").assertIsDisplayed()
         }
+    }
 }
